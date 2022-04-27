64773d6cacc6f1b4d10baa4fd934b977d918e823
fedelfeng <fedelfeng@tencent.com>
2022-04-27 13:08:42 +0800

    完善timex

--- timex/format.go
+++ timex/format.go
@@ -7,7 +7,6 @@ import (
 var layout = "2006-01-02 15:04:05"
 var dateLayout = "2006-01-02"
 
-
 // ParseTime 请使用String2Time
 func ParseTime(s string) (time.Time, error) {
 	return String2Time(s)
@@ -52,7 +51,6 @@ func Time2Unix(t time.Time) int64 {
 	return t.Unix()
 }
 
-
 // NowTimeString 返回当前字符串时间格式，如 2022-02-10 00:00:00
 func NowTimeString() string {
 	return time.Now().Format(layout)
@@ -61,4 +59,24 @@ func NowTimeString() string {
 // NowDateString 返回当前字符串日期格式，如 2022-02-10
 func NowDateString() string {
 	return time.Now().Format(dateLayout)
+}
+
+// NowUnix 返回当前的unix时间戳
+func NowUnix() int64 {
+	return time.Now().Unix()
+}
+
+// NowUnixMilli 返回当前的unix 毫秒
+func NowUnixMilli() int64 {
+	return time.Now().UnixMilli()
+}
+
+// NowUnixMicro 返回当前的unix 微秒
+func NowUnixMicro() int64 {
+	return time.Now().UnixMicro()
+}
+
+// NowUnixNano 返回当前的unix 纳秒
+func NowUnixNano() int64 {
+	return time.Now().UnixNano()
 }
