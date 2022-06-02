c7f62773ac9b4a6c8e837cfbaeb03544e9535903
fedelfeng <fedelfeng@tencent.com>
2022-06-02 09:28:45 +0800

    redisx新增判断err 是否为redis nil快捷函数

--- storex/redisx/redis.go
+++ storex/redisx/redis.go
@@ -126,7 +126,7 @@ func (p *Client) Exist(key string) (bool, error) {
 	return cmd.Val() == 1, cmd.Err()
 }
 
-func (p *Client) Do(args ...interface{}) *redis.Cmd{
+func (p *Client) Do(args ...interface{}) *redis.Cmd {
 	return p.client.Do(p.getCtx(), args...)
 }
 
@@ -138,4 +138,9 @@ func (p *Client) NewLocker(sourceID string, autoUnlockSecond int64) *Locker {
 // NewList 创建分布式队列
 func (p *Client) NewList(listName string) *List {
 	return NewList(p, listName)
+}
+
+// IsRedisNil 判断err是否为redis nil
+func IsRedisNil(err error) bool {
+	return err == redis.Nil
 }
