8639627cbf46bdcfe73ccd0e6b2a177d9e92679d
fedelfeng <fedelfeng@tencent.com>
2022-06-08 14:34:15 +0800

    feat: 新增stringsx增强包

---
+++ stringsx/safe_string.go
@@ -0,0 +1,12 @@
+package stringsx
+
+// SafeString 安全的返回字符串，如果str长度超过safeLength，返回str[0:safeLength]
+func SafeString(str string, safeLength ...int) string {
+	if len(safeLength) > 0 {
+		length := safeLength[0]
+		if len(str) >= length {
+			return str[0:length]
+		}
+	}
+	return str
+}
