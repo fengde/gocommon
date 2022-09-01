8991003f4f05bcd975fbc34cc3e55ca4da0a47d3
fedelfeng <fedelfeng@tencent.com>
2022-09-01 20:12:50 +0800

    update

--- ratelimitx/ratelimit_test.go
+++ ratelimitx/ratelimit_test.go
@@ -3,14 +3,24 @@ package ratelimitx
 import (
 	"testing"
 	"time"
+
+	"github.com/fengde/gocommon/timex"
 )
 
 func TestRatelimit_Run(t *testing.T) {
-	rl := NewRatelimit(2)
+	rl := NewRatelimit(3)
 	for {
 		rl.Run(func() {
-			t.Log("hello world")
+			t.Log(timex.NowTimeString(), "hello world")
 		})
-		time.Sleep(time.Millisecond)
+	}
+}
+
+func TestRatelimit2_Run(t *testing.T) {
+	rl := NewRatelimitCommon(3, 2*time.Second)
+	for {
+		rl.Run(func() {
+			t.Log(timex.NowTimeString(), "hello world")
+		})
 	}
 }
