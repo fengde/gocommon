f7f0622c9d2c105e5070485082e0b43aa2de9098
fedelfeng <fedelfeng@tencent.com>
2022-09-05 22:58:36 +0800

    slice去重支持

--- slicex/slice.go
+++ slicex/slice.go
@@ -26,3 +26,42 @@ func Int64Contains(arr []int64, item int64) bool {
 	}
 	return false
 }
+
+func StrRemoveRepeat(arr []string) []string {
+	var newArr []string
+	var m = map[string]int{}
+	for _, item := range arr {
+		if _, ok := m[item]; !ok {
+			newArr = append(newArr, item)
+			m[item] = 1
+		}
+	}
+
+	return newArr
+}
+
+func IntRemoveRepeat(arr []int) []int {
+	var newArr []int
+	var m = map[int]int{}
+	for _, item := range arr {
+		if _, ok := m[item]; !ok {
+			newArr = append(newArr, item)
+			m[item] = 1
+		}
+	}
+
+	return newArr
+}
+
+func Int64RemoveRepeat(arr []int64) []int64 {
+	var newArr []int64
+	var m = map[int64]int{}
+	for _, item := range arr {
+		if _, ok := m[item]; !ok {
+			newArr = append(newArr, item)
+			m[item] = 1
+		}
+	}
+
+	return newArr
+}
--- slicex/slice_test.go
+++ slicex/slice_test.go
@@ -1,7 +1,15 @@
 package slicex
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestContains(t *testing.T) {
 	t.Log(IntContains([]int{1, 2, 4, 5}, 2))
+}
+
+func TestRemoveRepeat(t *testing.T) {
+	t.Log(StrRemoveRepeat([]string{"a", "b", "a", "c", "c", "b", "d"}))
+	t.Log(IntRemoveRepeat([]int{1, 2, 3, 4, 5, 6, 4, 3, 2}))
+	t.Log(Int64RemoveRepeat([]int64{1, 2, 3, 4, 5, 6, 4, 3, 2}))
 }
