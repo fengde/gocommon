4d044080b5b9eb02954a41a44cb7f075cdcc4459
fedelfeng <fedelfeng@tencent.com>
2022-09-30 11:44:58 +0800

    feat: errorx规范

--- errorx/error.go
+++ errorx/error.go
@@ -16,22 +16,32 @@ func Errorf(format string, args ...interface{}) error {
 	return errors.Errorf(format, args...)
 }
 
-// WithStack 给err附带上Stack信息
+// WithStack 给err附带上stack信息，适用于报错”路径分析“
 func WithStack(err error) error {
 	return errors.WithStack(err)
 }
 
-// Wrap 给err附带上Stack信息和message信息
+// WithMessage 给err附带上message, 一般用于中间服务层逻辑
+func WithMessage(err error, message string) error {
+	return errors.WithMessage(err, message)
+}
+
+// WithMessagef 给err附带上message, 一般用于中间服务层逻辑
+func WithMessagef(err error, format string, args ...interface{}) error {
+	return errors.WithMessagef(err, format, args...)
+}
+
+// Wrap 给err附带上stack信息和message信息，一般用于底层结构
 func Wrap(err error, message string) error {
 	return errors.Wrap(err, message)
 }
 
-// Wrapf 给err附带上Stack信息和message信息
+// Wrapf 给err附带上stack信息和message信息，一般用于底层结构
 func Wrapf(err error, format string, args ...interface{}) error {
 	return errors.Wrapf(err, format, args...)
 }
 
-// Stack 返回错误堆栈信息
+// GetStack 返回错误堆栈信息，方便打印
 func GetStack(err error) string {
 	return fmt.Sprintf("%+v", err)
 }
--- proxyx/proxy.go
+++ proxyx/proxy.go
@@ -6,22 +6,6 @@ import (
 	"net/url"
 )
 
-// // 直接将请求代理到address
-// func Handler(address string) http.Handler {
-// 	u, _ := url.Parse(address)
-// 	proxy := httputil.NewSingleHostReverseProxy(u)
-
-// 	return &ProxyHandler{proxy: proxy}
-// }
-
-// type ProxyHandler struct {
-// 	proxy *httputil.ReverseProxy
-// }
-
-// func (p *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	p.proxy.ServeHTTP(w, r)
-// }
-
 // 直接将请求代理到address
 func HandlerFunc(address string) http.HandlerFunc {
 	u, _ := url.Parse(address)
